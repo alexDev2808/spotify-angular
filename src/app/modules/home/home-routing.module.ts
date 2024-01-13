823f6547ce8e949cd817adfe03011ca69daffcb5
alexDev2808 <colores.2808.8000@gmail.com>
2024-01-13 17:04:49 -0600

    Lazy loading

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
-  
+  {
+    path: '',
+    component: HomePageComponent
+  }
 ];
 
 @NgModule({

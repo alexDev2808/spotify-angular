d73b7bd016a63eabc8414adab2daee18b811d570
alexDev2808 <colores.2808.8000@gmail.com>
2024-01-14 10:25:52 -0600

    Estilos para card player, conectando con el modelo de tracks

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
   templateUrl: './card-player.component.html',
   styleUrls: ['./card-player.component.css']
 })
-export class CardPlayerComponent {
+export class CardPlayerComponent implements OnInit {
+
+  @Input() mode: 'small' | 'big' = 'small';
+  @Input() track: TrackModel = {_id: 0, name: '', album: '', url: '', cover: ''};
+
+  ngOnInit(): void {
+    
+  }
 
 }
